ab9b988fb752692043f7e0a15f334c6ad09257d1
Travis Lee <travisslee223@gmail.com>
2020-10-18 04:34:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Travis"
+  end
+  
+  get '/hometown' do
+    "My hometown is East Elmhurst"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is All I Want"
+  end
 end
